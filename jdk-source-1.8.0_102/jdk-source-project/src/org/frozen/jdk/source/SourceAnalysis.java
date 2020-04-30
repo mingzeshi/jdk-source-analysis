1b12075168d64e44bb8b9d17f604ead6437f13d6
shimingze <shimingze@jianlc.com>
2020-04-30 17:47:36 +0800

    jdk-1.8.0_102

---
+++ jdk-source-1.8.0_102/jdk-source-project/src/org/frozen/jdk/source/SourceAnalysis.java
@@ -0,0 +1,12 @@
+package org.frozen.jdk.source;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SourceAnalysis {
+
+    public static void main(String[] args) {
+        Map<String, String> hashMap = new HashMap<String, String>();
+        hashMap.put("a", "A");
+    }
+}
